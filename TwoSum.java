46b535997e5db26ce6172e41ad36b38324356c75
Laura0831 <46638810+Laura0831@users.noreply.github.com>
2025-01-15 13:21:48 -0700

    completed!

---
+++ TwoSum.java
@@ -0,0 +1,30 @@
+
+
+public class TwoSum {
+    public static void main(String[] args) {
+       int[] nums = {3,2,4};
+        
+       int[] result = twoSum(nums,6);
+       System.out.println(result.toString());
+    }
+
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] temp = {0,0};
+
+        for(int i = 0; i<nums.length-1; i++){
+            
+            for(int j = i+1; j<nums.length; j++){
+                
+                if(nums[i]+nums[j] == target){
+                    temp[0] = i; 
+                    temp[1] = j; 
+                    return temp;
+                }
+                
+            }
+            
+        }
+        return temp;
+    }
+}
